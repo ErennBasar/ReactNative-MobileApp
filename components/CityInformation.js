1760e89b20e7cf05728d606958016aa12729090f
Eren Basar <erenbasar16@hotmail.com>
2024-08-16 13:48:23 +0300

    şehir bilgilerinin tutan fonksiyon oluşturuldu

---
+++ components/CityInformation.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CityInformation = () => {
+  return (
+    <View>
+      <Text>deneme</Text>
+    </View>
+  )
+}
+
+export default CityInformation
+
+const styles = StyleSheet.create({})
