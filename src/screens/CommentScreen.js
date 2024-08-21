e8ec3a3863f55d9c9e2f5430ae520639d4e1cf6d
Eren Basar <erenbasar16@hotmail.com>
2024-08-21 11:20:32 +0300

    delete comment and render comment functions has been created

--- src/screens/CommentScreen.js
+++ src/screens/CommentScreen.js
@@ -1,27 +1,75 @@
-import { StyleSheet, Text, View ,TextInput,Button,FlatList, KeyboardAvoidingView,Platform,TouchableWithoutFeedback,Keyboard } from 'react-native'
-import React,{useState} from 'react'
+import { StyleSheet, Text, View ,TextInput,Button,ScrollView,TouchableOpacity,FlatList, KeyboardAvoidingView,Platform,TouchableWithoutFeedback,Keyboard } from 'react-native'
+import React,{useState,useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { auth } from '../../firebase';
 
 const CommentScreen = () => {
 
+
+const user = auth.currentUser;
+const fullName = user?.displayName || '';
+const username = user?.displayName || '';
+
 const [inputText, setInputText] = useState('');
-const[comment,setComment] = useState([])
+const [comment,setComment] = useState([])
+
+useEffect(() => {
+  loadComments();
+}, []);
+const clearAllAsyncStorage = async () => {
+  try {
+    await AsyncStorage.clear();
+    console.log('AsyncStorage tamamen temizlendi');
+  } catch (error) {
+    console.error('AsyncStorage temizlenirken bir hata oluştu:', error);
+  }
+};
+
+const loadComments = async () => {
+  try {
+      const storedComments = await AsyncStorage.getItem('comments');
+      if (storedComments !== null) {
+          setComment(JSON.parse(storedComments));
+      }
+  } catch (error) {
+      console.error('Failed to load comments:', error);
+  }
+};
+const saveComments = async (comments) => {
+  try {
+      await AsyncStorage.setItem('comments', JSON.stringify(comments));
+  } catch (error) {
+      console.error('Failed to save comments:', error);
+  }
+};
 
 const addComment = () => {
   if (inputText.trim() !== ''){  
-    setComment([...comment,inputText]);
+    const newComment = { id: Date.now().toString(),name: fullName,username: username, text: inputText };
+    const newComments = [...comment, newComment];
+    setComment(newComments);
+    saveComments(newComments);
     setInputText('');
   }
 }
+const deleteComment = (id) => {
+  const filteredComments = comment.filter(c => c.id !== id);
+  setComment(filteredComments);
+  saveComments(filteredComments);
+};
 const renderComment = ({ item }) => (
   <View style={styles.commentContainer}>
-    <Text>{item}</Text>
+    <Text style={styles.commentText}>{`${item.name} `}</Text>
+    <Text>{item.text}</Text>
+    {item.username === username && ( // Yalnızca kullanıcı kendi yorumlarını silebilsin
+    <TouchableOpacity onPress={() => deleteComment(item.id)} style={styles.deleteButton}>
+        <Text style={styles.deleteButtonText}>Delete</Text>
+      </TouchableOpacity>
+    )}
   </View>
 );
 
   return (
-
-
-
     <KeyboardAvoidingView 
         style={styles.container} 
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // iOS'te klavye çıktığında kaydırma yapar
@@ -32,9 +80,10 @@ const renderComment = ({ item }) => (
             <FlatList
             data={comment}
             renderItem={renderComment}   
-            keyExtractor={(_, index) => index.toString()}
+            keyExtractor={(item) => item.id}
             contentContainerStyle={styles.commentList}
             />
+          <Button title='Tüm Yorumları Sil' onPress={clearAllAsyncStorage}></Button> 
           <View style={styles.inputContainer}>
              <TextInput
              style={styles.input}
@@ -59,11 +108,11 @@ const styles = StyleSheet.create({
   },
   inner: {
     flex: 1,
-    justifyContent: 'space-between',
+    
   },
   commentList: {
     flexGrow: 1,
-    padding: 10,
+    
   },
   inputContainer: {
     flexDirection: 'row',
@@ -73,12 +122,7 @@ const styles = StyleSheet.create({
     marginVertical: 4,
     backgroundColor: '#ccc',
  },
- textContainer: {
-    flexDirection: 'row',
-    padding: 10,
-    borderTopWidth: 1,
-    borderColor: '#FF8343',
-},
+
 input: {
   flex: 1,
   borderWidth: 1,
@@ -91,5 +135,16 @@ commentContainer: {
   paddingVertical: 10,
   borderBottomWidth: 1,
   borderBottomColor: '#FF8343', // Ayırt edici çizgi rengi
+},
+commentText: {
+  fontWeight: 'bold',
+},
+deleteButton: {
+  backgroundColor: '#FF0000',
+  padding: 5,
+  borderRadius: 5,
+},
+deleteButtonText: {
+  color: '#FFF',
 },
 })
