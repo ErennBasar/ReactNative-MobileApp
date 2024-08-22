37c6dee3bf7542a7d755ca6b33b333c4733aed2f
Eren Basar <erenbasar16@hotmail.com>
2024-08-22 11:16:38 +0300

    components added

--- src/screens/PlaceDetailsScreen.js
+++ src/screens/PlaceDetailsScreen.js
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,Button,ScrollView,TouchableOpacity} from 'react-native'
 import React from 'react'
+import PlaceDetails from '../../components/PlaceDetails';
 
-const PlaceDetailsScreen = ({route}) => {
+const PlaceDetailsScreen = ({route,navigation}) => {
 
     const { placeName, placeInfo } = route.params;
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>{placeName}</Text>
       <Text style={styles.info}>{placeInfo}</Text>
-    </View>
+      { placeName === 'Didim' && 
+      <ScrollView>
+
+        <TouchableOpacity onPress={() => navigation.navigate('PlaceInfoScreen', {place:'Altınkum'})}>
+          <PlaceDetails 
+          title='Altınkum'
+          imageSource={require('../../assets/PlacesImages/didim-altınkum.jpeg')} 
+          desc='Altınkum, Didim ilçesine bağlı bir mahalledir.' 
+          
+          />
+        </TouchableOpacity>
+
+          <Button title="Konum Onayla" onPress={() => navigation.navigate('LocationVerificationScreen', {cityName: placeName})} />
+      </ScrollView>
+      }
+       
+    </ScrollView>
   )
 }
 
 export default PlaceDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  title:{
+    fontSize:30,
+    fontStyle:'bold',
+    textAlign:'center',
+  },
+  info:{
+    fontSize:20,
+    fontStyle:'arial',
+    textAlign:'center',
+  },
+  
+})
