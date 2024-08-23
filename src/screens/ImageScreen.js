4afa49c3d0a19bb07e66681941be809002703dc8
Eren Basar <erenbasar16@hotmail.com>
2024-08-23 14:53:42 +0300

    new images added

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View,TextInput ,Image } from 'react-native'
+import React,{useState} from 'react'
+import * as ImagePicker from 'expo-image-picker';
+import ImageViewer from '../../components/ImageViewer'; 
+import Button from '../../components/Button';
