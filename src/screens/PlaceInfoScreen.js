7c0727ca6150aa350c81d8fe2fe4856c49ae8dce
Eren Basar <erenbasar16@hotmail.com>
2024-08-23 16:32:58 +0300

    place comment screen button and upload foto button has been added

--- src/screens/PlaceInfoScreen.js
+++ src/screens/PlaceInfoScreen.js
@@ -1,64 +1,80 @@
-import { StyleSheet, Text, View,TextInput ,Image } from 'react-native'
+import { StyleSheet, Text, View,TextInput ,Button, TouchableOpacity } from 'react-native'
 import React,{useState} from 'react'
-import * as ImagePicker from 'expo-image-picker';
-import ImageViewer from '../../components/ImageViewer'; 
-import Button from '../../components/Button';
 
+const PlaceInfoScreen = ({route,navigation}) => {
 
-
-const PlaceholderImage = require('../../assets/PlacesImages/background-image.png');
-
-const PlaceInfoScreen = ({route}) => {
-
-    const { place  } = route.params || {};
-  
-    const [selectedImage, setSelectedImage] = useState(null);
-
-    const pickImageAsync = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: true,
-        quality: 1,
-      });
-  
-      if (!result.canceled) {
-        setSelectedImage(result.assets[0].uri);
-      } else {
-        alert('You did not select any image.');
-      }
-    };
-  return (
-        <View style={{ flex: 1, width: '100%', alignItems: 'center' }}>
-          <View style={styles.imageContainer}>
-            <ImageViewer  
-                placeholderImageSource={PlaceholderImage}
-                selectedImage={selectedImage}
-                />
-          </View>
-          <View style={styles.footerContainer}>
-              <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-              
-          </View>  
+  const {place} = route.params
+;  return (
+    <View>
+      <Text style={styles.title}>{place} Info Screen</Text>
+      {place == 'Akbük' &&
+        <View>
+          <TouchableOpacity 
+          style={styles.button}
+          onPress={() =>navigation.navigate('ImageScreen',{placeName:place} )}
+          >
+            <Text>Upload Foto</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+          style={styles.button}
+          onPress={() =>navigation.navigate('CommentScreen',{cityName:place} )}
+          >
+            <Text> {place} Comments</Text>
+          </TouchableOpacity>
         </View>
-      
-    
+      }
+      {place == 'Altınkum' &&
+        <View>
+          <TouchableOpacity 
+          style={styles.button}
+          onPress={() =>navigation.navigate('ImageScreen',{placeName:place} )}
+          >
+            <Text>Upload Foto</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+          style={styles.button}
+          onPress={() =>navigation.navigate('CommentScreen',{cityName:place} )}
+          >
+            <Text> {place} Comments</Text>
+          </TouchableOpacity>
+        </View>
+      }
+      {place == 'Dilek Yarımadası' &&
+        <View>
+          <TouchableOpacity 
+          style={styles.button}
+          onPress={() =>navigation.navigate('ImageScreen',{placeName:place} )}
+          >
+            <Text>Upload Foto</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+          style={styles.button}
+          onPress={() =>navigation.navigate('CommentScreen',{cityName:place} )}
+          >
+            <Text> {place} Comments</Text>
+          </TouchableOpacity>
+        </View>
+      }
+    </View>
   )
- }
+}
 
 export default PlaceInfoScreen
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+  button:{
+    backgroundColor:'#ccc', 
+    padding:10, 
+    margin:10,
+    alignItems:'center',
+    borderWidth:2,
+    borderColor:'#f53b57',
+    borderRadius:10,
   },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  title:{
+    fontSize:25,
+    textAlign:'center',
+    fontStyle:'bold',
   },
 })
+
